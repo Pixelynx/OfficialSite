34fa88521b0ff6c440eb834ea078d59448001a4d
pixelynx <tjstevens1992@gmail.com>
2020-03-22 23:57:12 -0400

    start jq slider

--- slider.js
+++ slider.js
@@ -1,3 +1,16 @@
 (function($){
 
+
+   // ==== BEGINS PLUGGIN ====
+   $.fn.techslider = function(data, callback){
+
+        // ==== CACHE DOM ====
+        var $techslider = $(this);
+        var $tsContent = $techslider.find('.tech-slider-inner');
+        var $tsNext = $techslider.find('button.TS-next-btn');
+        var $tsPrev = $techslider.find('button.MS-prev-btn');
+        var $imgsFirst = $msContent.find('.active');
+   }
+
+
 })(jQuery);
