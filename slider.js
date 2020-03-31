a7311886fb88428b208313f26e3f591465e10591
pixelynx <tjstevens1992@gmail.com>
2020-03-30 23:31:15 -0400

    add partial JQ slider logic

--- slider.js
+++ slider.js
@@ -1,15 +1,58 @@
-(function($){
+(function ($) {
 
 
-   // ==== BEGINS PLUGGIN ====
-   $.fn.techslider = function(data, callback){
+   // ==== BEGINS PLUGGIN ==== //
+   $.fn.techslider = function (data, callback) {
 
-        // ==== CACHE DOM ====
-        var $techslider = $(this);
-        var $tsContent = $techslider.find('.tech-slider-inner');
-        var $tsNext = $techslider.find('button.TS-next-btn');
-        var $tsPrev = $techslider.find('button.MS-prev-btn');
-        var $imgsFirst = $msContent.find('.active');
+      // ==== CACHE DOM ==== //
+      let $techslider = $(this);
+      let $tsContent = $techslider.find('.TS-content');
+      let $tsNext = $techslider.find('button.TS-next-btn');
+      let $tsPrev = $techslider.find('button.MS-prev-btn');
+      let $imgCurrent = $msContent.find('.active');
+
+      // ==== VARIABLES ==== //
+      let animateSlideNext,
+      animateSlidePrev;
+
+      // ==== EVENT HANDLERS ==== //
+      $tsNext.on('click', animateSlidePrev);
+      $tsPrev.on('click', animateSlideNext);
+      $multislider.on('click','.TS-prev-btn, .TS-next-btn');
+
+      // ==== FUNCTIONS ==== //
+
+      function isItAnimating(callback) {
+         if (!$techslider.hasClass('active')) {
+            //   $techslider.trigger('ms.before.animate'); // event!
+            $techslider.addClass('active');
+            callback();    //callback is animation
+         }
+      };
+
+      function selectAnimation() {
+         animateSlideNext = $techslider.data('prev');
+         animateSlidePrev = $techslider.data('next');
+      };
+
+      function slideLeft() {
+         isItAnimating(function () {
+            // reTargetSlides();
+            $imgCurrent.animate(
+               {
+                  marginLeft: "-100px"
+               }, {
+               duration: "1s",
+               easing: "swing",
+               complete: function () {
+                  $imgCurrent.detach().addClass('hide').removeClass('active').appendTo($tsContent);
+                  // doneAnimating();
+               }
+            }
+            );
+         });
+      };
+      return $techslider;
    }
 
 
