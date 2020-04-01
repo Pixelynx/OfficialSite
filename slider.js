59fb8c608d37631a63636fdc488dd279fb42fa4c
pixelynx <tjstevens1992@gmail.com>
2020-03-31 23:29:17 -0400

    add jQ functioning with event handling bugs

--- slider.js
+++ slider.js
@@ -1,26 +1,49 @@
-(function ($) {
+(function($) {
 
 
    // ==== BEGINS PLUGGIN ==== //
-   $.fn.techslider = function (data, callback) {
+   $.fn.techslider = function(data, callback) {
 
       // ==== CACHE DOM ==== //
       let $techslider = $(this);
       let $tsContent = $techslider.find('.TS-content');
       let $tsNext = $techslider.find('button.TS-next-btn');
       let $tsPrev = $techslider.find('button.MS-prev-btn');
-      let $imgCurrent = $msContent.find('.active');
+      let $imgCurrent = $tsContent.find('.active');
 
       // ==== VARIABLES ==== //
       let animateSlideNext,
-      animateSlidePrev;
+         animateSlidePrev;
+
+      // === DETERMINE ACTION ====
+      // string = method | object or nothing is to initialize
+      if (typeof data === 'string') {
+         getStringArgs(data);
+         return $multislider;
+      } else if (typeof data === 'object' || typeof data === 'undefined') {
+         init();
+      };
+
+      // ==== INITIALIZE ==== //
+      function init() {
+         selectAnimation();
+      };
 
       // ==== EVENT HANDLERS ==== //
       $tsNext.on('click', animateSlidePrev);
       $tsPrev.on('click', animateSlideNext);
-      $multislider.on('click','.TS-prev-btn, .TS-next-btn');
+      $techslider.on('click', '.TS-prev-btn, .TS-next-btn');
 
       // ==== FUNCTIONS ==== //
+
+      // used if method is called after initialization
+      function getStringArgs(str) {
+         if (typeof $techslider.data(str) !== 'undefined') {
+            $techslider.data(str)();
+         } else {
+            console.error("Techslider currently only accepts the following methods: next, prev")
+         }
+      }
 
       function isItAnimating(callback) {
          if (!$techslider.hasClass('active')) {
